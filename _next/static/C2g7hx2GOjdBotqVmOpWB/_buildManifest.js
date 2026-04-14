4cfb9835468f2fc1faebc6b7547d46db36e889b7
csy214-beep <csy214-beep@users.noreply.github.com>
2026-04-14 15:11:14 +0000

    deploy: 98f333b602c070390af7cd907e5bab61920f1456

--- _next/static/C2g7hx2GOjdBotqVmOpWB/_buildManifest.js
+++ _next/static/C2g7hx2GOjdBotqVmOpWB/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Pblog//_next/:path+",
+        "destination": "/Pblog/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
