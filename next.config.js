c4e32ca27c4805028407f07cf8e01bf7ef7e5e9e
csy214-beep <csy2146047622@gmail.com>
2026-04-29 13:13:24 +0800

    fix: path next.config.js

--- next.config.js
+++ next.config.js
@@ -6,8 +6,6 @@ const nextConfig = {
   output: "export", // 开启纯静态导出
   trailingSlash: true, // 路径统一带/，避免路由异常
   distDir: "output",
-  basePath: "/FunRadiusP", // 替换成你的仓库名
-  assetPrefix: "/FunRadiusP/", // 注意末尾的斜杠
   // Next.js 会自动处理 basePath，无需手动加 /Pblog。但如果你有直接写在 <a href="/xxx"> 里的路径，需要改成 <a href={"/Pblog/xxx"}> 或使用 next/link。最好全部改用 next/link 和 next/router。
 };
 
